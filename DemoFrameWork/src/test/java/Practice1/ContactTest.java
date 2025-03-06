4e1821d5b168907141dae0f59163497ca8613f65
rajanand9852 <romilraj9852@gmail.com>
2025-03-06 17:38:11 +0530

    commit

---
+++ DemoFrameWork/src/test/java/Practice1/ContactTest.java
@@ -0,0 +1,31 @@
+package Practice1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createcontactTest() {
+		
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String PASSWORD = System.getProperty("password");
+		String USERNAME = System.getProperty("username");
+		
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(PASSWORD);
+		System.out.println(USERNAME);
+		
+		
+		
+		
+	}
+	@Test
+	public void modifycontactTest() {
+		
+		System.out.println("execute modifycontact");
+	}
+
+}
