b9ba3cbd1dbc6366896f49f688deeac3fc0b799e
rajanand9852 <romilraj9852@gmail.com>
2025-03-10 11:14:25 +0530

    Commit

--- DemoFrameWork/src/test/java/Practice1/ContactTest.java
+++ DemoFrameWork/src/test/java/Practice1/ContactTest.java
@@ -1,6 +1,7 @@
 package Practice1;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
 public class ContactTest {
 	
--- DemoFrameWork/src/test/java/Practice2/OrgTest.java
+++ DemoFrameWork/src/test/java/Practice2/OrgTest.java
@@ -1,10 +1,22 @@
 package Practice2;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
 public class OrgTest {
 	@Test
 	public void createOrgTest() {
+		
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String PASSWORD = System.getProperty("password");
+		String USERNAME = System.getProperty("username");
+		
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(PASSWORD);
+		System.out.println(USERNAME);
 		System.out.println("execute createOrgTest");
 	}
 	
